3863fe13e3efbf0be3d15628541c59e47fd1d732
Lucas <lucasfigrock@gmail.com>
2022-07-28 05:51:21 -0300

    Corrigindo bugs do carrossel

--- GameMania/src/app/app.module.ts
+++ GameMania/src/app/app.module.ts
@@ -4,11 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { HeaderComponent } from './views/header/header.component';
+import { ArticleComponent } from './views/article/article.component';
+import { SlidesComponent } from './views/slides/slides.component';
+import { BannerPromocoesComponent } from './views/banner-promocoes/banner-promocoes.component';
+import { SlidesSmartphoneComponent } from './views/slides-smartphone/slides-smartphone.component';
+import { BannerNovidadesComponent } from './views/banner-novidades/banner-novidades.component';
+import { SlidesAcessoriosComponent } from './views/slides-acessorios/slides-acessorios.component';
+import { FooterComponent } from './views/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    ArticleComponent,
+    SlidesComponent,
+    BannerPromocoesComponent,
+    SlidesSmartphoneComponent,
+    BannerNovidadesComponent,
+    SlidesAcessoriosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ GameMania/src/app/views/slides-smartphone/slides-smartphone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slides-smartphone',
+  templateUrl: './slides-smartphone.component.html',
+  styleUrls: ['./slides-smartphone.component.css']
+})
+export class SlidesSmartphoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
